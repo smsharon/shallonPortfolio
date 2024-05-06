04341db7155452fc7f724be6559b9dc002432356
shallon <shallonmokeira@gmail.com>
2024-05-06 19:21:24 +0300

    add link tags

--- portfolio/src/components/Navbar/navbar.js
+++ portfolio/src/components/Navbar/navbar.js
@@ -8,7 +8,10 @@ const Navbar = () => {
     <nav className='navbar'>
         <img src={logo} alt='Logo' className='logo' />
         <div className='desktopMenu'>
-            <Link></Link>
+            <Link className="desktopMenuListItem">Home</Link>
+            <Link className="desktopMenuListItem">About</Link>
+            <Link className="desktopMenuListItem">Portfolio</Link>
+            <Link className="desktopMenuListItem">Contact</Link>
 
         </div>
         <button className='desktopMenuBtn'></button>
