871934ffa8e4b8d452a8a1e5ac54627170a91492
shallon <shallonmokeira@gmail.com>
2024-05-07 10:19:35 +0300

    style about section

--- portfolio/src/components/Intro/intro.js
+++ portfolio/src/components/Intro/intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
   return (
     <section id='intro'>
         <div className='introContent'>
-            <span className='hello'>Hello</span>
+            <span className='hello'>Hello,</span>
             <span className='introText'> I'm <span className='introName'>Shallon Said</span> <br />Software Engineer</span>
             <p className='introPara'>I am a skilled software engineer with experience in creating web and mobile applications.</p>
 
