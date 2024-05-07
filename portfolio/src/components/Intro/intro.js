6fc224a347c17db118fad088cd35c427e3e90adb
shallon <shallonmokeira@gmail.com>
2024-05-07 10:06:21 +0300

    add about

--- portfolio/src/components/Intro/intro.js
+++ portfolio/src/components/Intro/intro.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './intro.css';
+import bg from '../../assets/logo.jpg'
 
 const Intro = () => {
   return (
-    <div>intro</div>
+    <section id='intro'>
+        <div className='introContent'>
+            <span className='hello'>Hello</span>
+            <span className='introText'> I'm <span className='introName'>Shallon Said</span> <br />Software Engineer</span>
+            <p className='introPara'>I am a skilled software engineer with experience in creating web and mobile applications.</p>
+
+        </div>
+        <img src={bg} alt='profile' className='bg' />
+        
+
+    </section>
   )
 }
 
