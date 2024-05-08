92b9961c0fcf4a082f01e9962ee459f20297f24d
shallon <shallonmokeira@gmail.com>
2024-05-08 20:55:07 +0300

    modify styling

--- portfolio/src/components/Intro/intro.js
+++ portfolio/src/components/Intro/intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
         <div className='introContent'>
             <span className='hello'>Hello,</span>
             <span className='introText'> I'm <span className='introName'>Shallon Said</span> <br />Software Engineer</span>
-            <p className='introPara'>I am a skilled software engineer with experience in creating web and mobile applications.</p>
+            <p className='introPara'>I am a skilled software engineer with experience in <br />creating web applications and mobile applications.</p>
 
         </div>
         <img src={bg} alt='profile' className='bg' />
