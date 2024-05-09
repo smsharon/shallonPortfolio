05fc6d8ca61cc2bb00a469aa420bdf56c421f4d8
shallon <shallonmokeira@gmail.com>
2024-05-09 17:53:43 +0300

    add skills section

---
+++ portfolio/src/components/Skills/skills.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './skills.css';
+
+const Skills = () => {
+  return (
+    <section id='skills'>
+        About Section
+
+    </section>
+  )
+}
+
+export default Skills
