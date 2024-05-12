020e1f863dea27baecfa440f1e76e7cf26fdf590
shallon <shallonmokeira@gmail.com>
2024-05-12 18:31:18 +0300

    add sections

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -4,7 +4,8 @@ import './skills.css';
 const Skills = () => {
   return (
     <section id='skills'>
-        About Section
+        <span className='skillTitle'>What I do</span>
+        <span className='skillDesc'>I am a skilled and passionate Full-stack Software Engineer with experience in creating visually appealing and user friendly web and mobile applications. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML,CSS,Javascript, ReactJs, Node, Python, Flask, React Native, Firebase, Expo, Figma and SQl.</span>
 
     </section>
   )
