da3b4a042c408f9714acb43b3446b93d1b430d5d
shallon <shallonmokeira@gmail.com>
2024-05-14 16:36:30 +0300

    add images

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -8,21 +8,21 @@ const Skills = () => {
         <span className='skillDesc'>I am a skilled and passionate Full-stack Software Engineer with experience in creating visually appealing and user friendly web and mobile applications. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML,CSS,Javascript, ReactJs, Node, Python, Flask, React Native, Firebase, Expo, Figma and SQl.</span>
         <div className='skillBars'>
           <div className='skillBar'>
-            <img src='' alt='' className='skillBarImg' />
+            <img src='' alt='UIDesign' className='skillBarImg' />
             <div className='skillBarText'>
               <h2>lorem</h2>
               <p></p>
             </div>
           </div>
           <div className='skillBar'>
-            <img src='' alt='' className='skillBarImg' />
+            <img src='' alt='webDesign' className='skillBarImg' />
             <div className='skillBarText'>
               <h2>lorem</h2>
               <p></p>
             </div>
           </div>
           <div className='skillBar'>
-            <img src='' alt='' className='skillBarImg' />
+            <img src='' alt='AppDesign' className='skillBarImg' />
             <div className='skillBarText'>
               <h2>lorem</h2>
               <p></p>
